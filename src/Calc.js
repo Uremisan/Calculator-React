2c604331007f8db675a505226f3afc85742eaea2
Marley <manfrededun@gmail.com>
2021-03-20 08:15:46 +0000

    first initialization

---
+++ src/Calc.js
@@ -0,0 +1,101 @@
+function Calc () {
+  return (
+<div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <p>0</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-3">
+        <p>AC</p>
+      </div>
+
+      <div class="col-md-3">
+        <p>+/-</p>
+      </div>
+
+      <div class="col-md-3">
+        <p>%</p>
+      </div>
+
+      <div class="col-md-3 strain">
+        <p>÷</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-3">
+        <p>7</p>
+      </div>
+
+      <div class="col-md-3">
+        <p>8</p>
+      </div>
+
+      <div class="col-md-3">
+        <p>9</p>
+      </div>
+
+      <div class="col-md-3 strain">
+        <p>×</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-3">
+        <p>4</p>
+      </div>
+
+      <div class="col-md-3">
+        <p>5</p>
+      </div>
+
+      <div class="col-md-3">
+        <p>6</p>
+      </div>
+
+      <div class="col-md-3 strain">
+        <p>-</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-3">
+        <p>1</p>
+      </div>
+
+      <div class="col-md-3">
+        <p>2</p>
+      </div>
+
+      <div class="col-md-3">
+        <p>3</p>
+      </div>
+
+      <div class="col-md-3 strain">
+        <p>+</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6" >
+        <p>0</p>
+
+      </div>
+      <div class="col-md-3 flip">
+        <p>.</p>
+
+      </div>
+
+      <div class="col-md-3 strain flip">
+        <p>=</p>
+
+      </div>
+    </div>
+
+  </div>
+  )
+}
+export default Calc
